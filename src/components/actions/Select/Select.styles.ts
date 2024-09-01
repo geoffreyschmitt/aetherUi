1ade3a6c993b28e8fd78063c5f5abe41594c18fe
Geoffrey <geoffrey.schmitt.pro@gmail.com>
2024-09-01 23:06:44 +0200

    feat: Create Select component

---
+++ src/components/actions/Select/Select.styles.ts
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+
+export const RootElement = styled.div`
+  position: relative;
+
+  .select__select {
+    display: none;
+  }
+
+  .select__trigger {
+    position: relative;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+  }
+
+  .select__value-icon {
+    transform: rotate(180deg);
+    transition: all 0.3s ease-in-out;
+  }
+
+  .select__option-list,
+  .select__option {
+    width: 100%;
+  }
+
+  .select__option-list {
+    display: none;
+  }
+
+  .select__option {
+    background-color: white;
+    &:hover,
+    &--focused {
+      background-color: lightblue;
+    }
+  }
+
+  &.select__is-open {
+    .select__option-list {
+      display: block;
+      position: absolute;
+      top: 100%;
+    }
+
+    .select__value-icon {
+      transform: rotate(0deg);
+    }
+  }
+`;
---
+++ src/components/actions/Select/mocks/selectOptionListMock.ts
@@ -0,0 +1,18 @@
+export const selectOptionListMock = [
+  {
+    value: 'value',
+    label: 'label',
+  },
+  {
+    value: 'value 2',
+    label: 'label 2',
+  },
+  {
+    value: 'value 3',
+    label: 'label 3',
+  },
+  {
+    value: 'value 4',
+    label: 'label 4',
+  },
+];
