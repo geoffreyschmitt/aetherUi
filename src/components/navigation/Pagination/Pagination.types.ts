cc7d21cff0dfb6da10b3a0499e1d216774ed83b6
Geoffrey <geoffrey.schmitt.pro@gmail.com>
2024-09-06 23:51:49 +0200

    feat: Create pagination component

---
+++ src/components/navigation/Pagination/Pagination.types.ts
@@ -0,0 +1,17 @@
+import { TComponentProps } from '@/utils';
+
+export type TPagination = TComponentProps &
+  Readonly<{
+    /**
+     * Current page index
+     */
+    currentPageIndex: number;
+    /**
+     * List of link displayed by the component.
+     */
+    totalPages: number;
+    /**
+     * Callback on page change
+     */
+    onPageChange: (pageNumber: number) => void;
+  }>;
