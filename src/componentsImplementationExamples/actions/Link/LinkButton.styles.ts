50f84e13adf721ee6903156e95511bce3d168cb0
Geoffrey <geoffrey.schmitt.pro@gmail.com>
2024-09-15 22:32:04 +0200

    chore: Create Link implementation example

---
+++ src/componentsImplementationExamples/actions/Link/LinkButton.styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import { LinkButton } from '@/components/actions/Link/LinkButton';
+import { LinkGlobalStyle } from './Link.global.styles';
+
+export const RootElement = styled(LinkButton)`
+  ${LinkGlobalStyle}
+`;
