e6e9c69e190ad74bf6ca6c6038dc414d7cf4c2c6
Geoffrey <geoffrey.schmitt.pro@gmail.com>
2024-09-15 22:40:08 +0200

    chore: Create Radio implementation example

---
+++ src/componentsImplementationExamples/actions/Radio/Radio.tsx
@@ -0,0 +1,9 @@
+'use client';
+import React from 'react';
+
+import { RootElement } from './Radio.styles';
+import { TRadio } from './Radio.types';
+
+export const Radio = ({ ...props }: TRadio) => {
+  return <RootElement {...props} />;
+};
