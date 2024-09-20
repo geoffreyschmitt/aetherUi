e428fe8c157289e29ad6f2a9ce2ce734856f5dd3
Geoffrey <geoffrey.schmitt.pro@gmail.com>
2024-09-21 01:24:59 +0200

    Add Container component with type definitions and tests
    This commit introduces the new Container component, along with its associated type definitions, styles, Storybook stories, and tests. The component is designed to allow customization through optional tag and className props and includes accessibility checks.

---
+++ src/componentsImplementationExamples/layouts/Container/Container.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Container as Component } from '.';
+import CoreContainerMeta, {
+  Container as CoreContainer,
+} from '@/components/layouts/Container/Container.stories';
+
+const meta: Meta<typeof Component> = {
+  title: 'Implementation Example/layouts/Container',
+  component: Component,
+  argTypes: {
+    ...CoreContainerMeta.argTypes,
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Component>;
+
+export const Container: Story = {
+  args: {
+    ...CoreContainer.args,
+  },
+};
