a22c9dc9780dd4da6be85f10eeba897044478a4d
Geoffrey <geoffrey.schmitt.pro@gmail.com>
2024-09-21 19:04:06 +0200

    Remove Button import from Tabs.tsx
    The Button component was removed from the dependencies of the Tabs component. This cleanup simplifies the Tabs component and reduces unnecessary imports.

--- src/componentsImplementationExamples/dataDisplay/Tabs/Tabs.tsx
+++ src/componentsImplementationExamples/dataDisplay/Tabs/Tabs.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 import { RootElement } from './Tabs.styles';
 import { TTabs } from './Tabs.types';
-import { Button } from '@/componentsImplementationExamples/actions/Button';
 
 export const Tabs = ({ ...props }: TTabs) => {
-  return <RootElement {...props} ButtonComponent={Button} />;
+  return <RootElement {...props} />;
 };
