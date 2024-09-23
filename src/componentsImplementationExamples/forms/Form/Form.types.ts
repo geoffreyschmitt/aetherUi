65385bc143c432cc873ecd76e1167fa66a5400c1
Geoffrey Schmitt <geoffrey.schmitt.pro@gmail.com>
2024-09-23 10:58:41 +0200

    Refactor Form component to support content slots
    Updated the Form component to include optional slots for rendering content before and after the form entries. Adjusted the type definitions and storybook configuration to reflect these changes, ensuring better flexibility and customization.

--- src/componentsImplementationExamples/forms/Form/Form.types.ts
+++ src/componentsImplementationExamples/forms/Form/Form.types.ts
@@ -1,3 +1,10 @@
 import { TForm as TCoreForm } from '@/components/forms/Form';
 
-export type TForm = TCoreForm;
+export type TForm = Omit<
+  TCoreForm,
+  | 'InputComponent'
+  | 'CheckboxComponent'
+  | 'RadioComponent'
+  | 'RadioListComponent'
+  | 'SelectComponent'
+>;
